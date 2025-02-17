41176c6152e9500b2bfbc4df14a8461c21cf5d6d
Рогов Олександр <RogovAlex@AREON.LOCAL>
2025-02-18 01:16:26 +0200

    result

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,10 +5,10 @@ import s from "./Navigation.module.css";
 export const Navigation = () => {
     return (
         <nav className={s.container}>
-            <Link to="/qr-code-l1/generate">Генерировать QR код</Link>
-            <Link to="/qr-code-l1/scan">Сканировать QR код</Link>
-            <Link to="/qr-code-l1/generateHistory">История генерирования</Link>
-            <Link to="/qr-code-l1/scanHistory">История сканирования</Link>
+            <Link to="/qr-code-l1/generate">Згенерувати QR код</Link>
+            <Link to="/qr-code-l1/scan">Сканувати QR код</Link>
+            <Link to="/qr-code-l1/generateHistory">Історія генерування</Link>
+            <Link to="/qr-code-l1/scanHistory">Історія сканування</Link>
 
         </nav>
     )
